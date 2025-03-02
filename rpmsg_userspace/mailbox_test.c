8171147d4f4d3c4a1b602f21a447a82957e88aa3
HenryChaing <n96121210@gs.ncku.edu.tw>
2025-03-02 12:08:46 +0800

    Add RPMsg Userspace Example
    This example depends on the ThreadX application. The remote side receives
    messages sent from this process and responds by sending a message back to
    it.

---
+++ rpmsg_userspace/mailbox_test.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include "rpmsg.h"
+#include <unistd.h>
+
+struct msg_data {
+    char databuf[45];
+};
+
+typedef struct msg_data msg_data_t;
+
+int main()
+{
+/** user-space application source code
+  * example channel: rpmsg-openamp-demo-channel [src=0x2 ----&gt; dst=0x1]
+  */
+
+    int status;
+    msg_data_t data_buf;
+
+    struct rpmsg_endpoint_info ept_info = {"rpmsg-openamp-demo-channel", 0x29, 0x1f};
+    int fd = open("/dev/rpmsg_ctrl0", O_RDWR);
+
+    /* create endpoint interface */
+    ioctl(fd, RPMSG_CREATE_EPT_IOCTL, &ept_info);  // /dev/rpmsg0 is created 
+
+    /* create endpoint */
+    int fd_ept = open("/dev/rpmsg0", O_RDWR); // backend creates endpoint
+    
+    char test_arr [26][45];
+
+    for (int i = 0; i < 26; i++)
+    {
+        for (int j = 0; j < 45; j++)
+        {
+            test_arr[i][j] = 'A' + i;
+        }
+    }
+
+    /* send data to remote device */ 
+    for (int i = 0; i < 26; i++)
+    {
+        status = write(fd_ept, test_arr[i], sizeof(char) * 45);
+        if (status) {
+            printf("the write status : %d\n", status);
+        }  
+    }  
+
+    /* receive data from remote device */
+    for (int i = 0; i < 26; i++)
+    {    
+        status = read(fd_ept, &data_buf.databuf, sizeof(data_buf.databuf));
+        if (status) {
+            printf("the read status : %d\n", status);
+        }
+        printf("%s\n",data_buf.databuf);
+    }   
+
+    /* destroy endpoint */
+    ioctl(fd_ept, RPMSG_DESTROY_EPT_IOCTL);
+
+    close(fd_ept);
+    close(fd);
+
+    return 0;
+}
+
